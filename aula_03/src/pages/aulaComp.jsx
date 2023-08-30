c5a454dab8b15eb7f8c5473fa2bf0766cf791bbf
carolbarto <caroline.barto@fatec.sp.gov.br>
2023-08-30 20:21:48 -0300

    Components

---
+++ aula_03/src/pages/aulaComp.jsx
@@ -0,0 +1,15 @@
+import PrimeiroComponente from "@/components/primeiroComp";
+
+export default function AulaComp(){
+    return(
+        <>
+            <PrimeiroComponente
+                titulo="Meu primeiro Componente"
+                subtitulo="Estamos aprendendo NEXT.js com REACT"
+            />
+            <ValidaCPF
+                
+            />
+        </>
+    )
+}
